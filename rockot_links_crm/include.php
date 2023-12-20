b5e89b71ebc89be95c72bf7fd3a872393a7dd558
vitaly.perov <vitaly.perov@ringcentral.com>
2023-12-20 14:55:01 +0300

    Refactor

--- rockot_links_crm/include.php
+++ rockot_links_crm/include.php
@@ -1,15 +1,19 @@
 <?
 use Bitrix\Main\Page\Asset;
 use Bitrix\Main;
-use  Bitrix\Disk;
+use Bitrix\Disk;
 
 Main\Loader::IncludeModule('disk');
+
+const UF_GROUP = 'UF_CRM_1679410842';
+const UF_DISK = 'UF_CRM_1679410808';
+
 
 class CRockotEventHandlers
 {
 	public static function OnBeforeProlog()
 	{
-		$type = CRockotEventHandlers::getPageType();
+		$type = RockotRequestHelper::getPageType();
 		if ($type == 'deal') {
 			CRockotEventHandlers::modifyDealPage();
 			return;
@@ -22,135 +26,96 @@ class CRockotEventHandlers
 
 	public static function OnProlog() {}
 
-	//------
+	//---------------------------------------
+	// Group page
+	//---------------------------------------
 	public static function modifyGroupPage() {
-		$groupId = CRockotEventHandlers::getPageId();
-		$isAjaxRequest = CRockotEventHandlers::isAjaxRequest();
+		$groupId = RockotRequestHelper::getPageId();
+		$isAjaxRequest = RockotRequestHelper::isAjaxRequest();
 		if (!$groupId || $isAjaxRequest) {
 			return;
 		}
-		//----
 		$deal = getItemById($groupId);
-		// getDiskByGroupId($groupId);
-		//---
 		if ($deal) {
-			CRockotEventHandlers::addLinkToGroupMenu("/crm/deal/details/".$deal["ID"]."/", "Сделка");
+			RockotGroup::addLinkToGroupMenu("/crm/deal/details/".$deal["ID"]."/", "Сделка");
 		}
-		// CRockotEventHandlers::addLinkToGroupMenu("/workgroups/group/$groupId/disk/path/", "Диск");
-		// CRockotEventHandlers::addLinkToGroupMenu("/online/", "Чат");
-		Asset::getInstance()->addJs("/bitrix/js/rockot_links_crm/script.js", true);
+
+		// Asset::getInstance()->addJs("/bitrix/js/rockot_links_crm/script.js", true);
+
 	}
-	//------
+
+	//---------------------------------------
+	// Deal page
+	//---------------------------------------
 	public static function modifyDealPage() {
 		CModule::IncludeModule('crm');
 
-		$dealId = CRockotEventHandlers::getPageId();
-		$isDealIframe = CRockotEventHandlers::isDealIframe();
-		$isAjaxRequest = CRockotEventHandlers::isAjaxRequest();
+		$dealId = RockotRequestHelper::getPageId();
+		$isDealIframe = RockotRequestHelper::isPageInIframe();
+		$isAjaxRequest = RockotRequestHelper::isAjaxRequest();
+		
 		if (!$dealId || !$isDealIframe || $isAjaxRequest) {
 			return;
 		}
 
-		$links = getDealUF($dealId);
+		$links = RockotDeal::getGroupAndDiskLinksByDeal($dealId);
 
 		if ($links["group"]) {
-			CRockotEventHandlers::addLinkToMenu($links["group"], "Проект");
+			RockotDeal::addLinkToMenu($links["group"], "Проект");
 		}
 		if ($links["disk"]) {
-			CRockotEventHandlers::addLinkToMenu($links["disk"], "Диск");	
+			RockotDeal::addLinkToMenu($links["disk"], "Диск");	
 		}
 		
-		$bpList = getBpListByDeal($dealId);
+		$bpList = RockotDeal::getBpListByDealId($dealId);
 
-		// var_dump($bpList);die();
+		// Asset::getInstance()->addJs("/bitrix/js/rockot_links_crm/script.js", true);
 
-		Asset::getInstance()->addJs("/bitrix/js/rockot_links_crm/script.js", true);
-
-		//-----
-		// echo "<pre>";
-		// CModule::IncludeModule('crm');
-
-		// $dbRes = CCrmDeal::GetListEx(
-		// 		[],
-		// 		["ID" => $dealId], // или другой фильтр, соответствующий вашим требованиям
-		// 		false,
-		// 		false,
-		// 		// ["ID", "TITLE", "STAGE_ID", "CATEGORY_ID"] // перечислите нужные поля
-		// 		[
-		// 			"UF_CRM_1679410842", // Group link
-		// 			"UF_CRM_1679410808" // Disk link
-		// 			// "UF_CRM_6419D566E9A8A", // Disk link 2
-		// 			// "UF_CRM_6419D56724C56", // Group link 2
-		// 		]
-		// );
-		// while ($deal = $dbRes->Fetch()) {
-		// 		// Обработка каждой сделки
-		// 		// echo "Сделка: " . $deal['TITLE'] . "<br>";
-		// 		var_dump($deal);
-		// }
-
-
-		// $deal = CCrmDeal::GetByID($dealId);
-		
-		// var_dump($deal);
-		// echo "</pre>";
 	}
+}
 
-	//------
-	public static function addLinkToGroupMenu($link, $title) {
-		?>
-		<script>
-        document.addEventListener('DOMContentLoaded', function() {
-						const customCardHtml = `<?=CRockotEventHandlers::getGroupLinkTemplate($link, $title)?>`;
-						const container = document.querySelector(".main-buttons-inner-container");
-						console.log(container);
-						if (container) {
-							container.insertAdjacentHTML('beforeend', customCardHtml);
-						}
-        });
-    </script>
-		<?
+function getItemById($groupId) {
+	/*
+	if (CModule::IncludeModule('tasks') && CModule::IncludeModule('crm')) {
+    $dbRes = CTasks::GetList(
+        [],
+        ["GROUP_ID" => $groupId],
+        ["UF_CRM_TASK"]
+    );
+    while ($task = $dbRes->Fetch()) {
+				foreach ($task['UF_CRM_TASK'] as $crmElement) {
+					if (strpos($crmElement, 'D_') === 0) {
+							$dealId = substr($crmElement, 2);
+							$deal = CCrmDeal::GetByID($dealId);
+							if ($deal) {
+									return $deal;
+							}
+					}
+			}
+    }
 	}
+	return null;
+	*/
+	if (CModule::IncludeModule('crm')) {
+    $filter = ['UF_CRM_1679410842' => $dealId]; // Фильтр по пользовательскому полю
+    $select = ['ID', 'TITLE', 'UF_CRM_1679410842']; // Поля, которые вы хотите получить
 
-	public static function addLinkToMenu($link, $title) {
-		?>
-		<script>
-        document.addEventListener('DOMContentLoaded', function() {
-						const customCardHtml = `<?=CRockotEventHandlers::getDealLinkTemplate2($link, $title)?>`;
-						// const container = document.querySelector("#crm_scope_detail_c_deal_");
-						const container = document.querySelector(".main-buttons-inner-container");
-						// console.log(container);
-						// console.log(customCardHtml);
-						if (container) {
-							container.insertAdjacentHTML('afterbegin', customCardHtml);
-						}
-        });
-    </script>
-		<?
+    $dbRes = CCrmDeal::GetListEx([], $filter, false, false, $select);
+    while ($deal = $dbRes->Fetch()) {
+        // Обработка результатов
+        echo "ID: " . $deal['ID'] . "; Название: " . $deal['TITLE'] . "; Пользовательское поле: " . $deal['UF_CRM_1679410842'] . "<br>";
+    }	
 	}
+	return null;
+}
 
+///-----
 
-	public static function isDealIframe() {
-		$request = \Bitrix\Main\Context::getCurrent()->getRequest();
-		$qwe = $request->getQueryList()->toArray();
-		return $qwe["IFRAME"] == "Y";
-	}
+class RockotRequestHelper {
 
-	public static function isAjaxRequest() {
-		if (isset($_REQUEST["ajax_request"]) && $_REQUEST["ajax_request"] === "Y") {
-			return true;
-		}
-		return false;
-	}
-
-	public static function getPageType() {
-		return CRockotEventHandlers::getInfoByURL()["type"];
-	}
-
-	public static function getPageId() {
-		return CRockotEventHandlers::getInfoByURL()["id"];
-	}
-
+	/**
+	 * Get type and id for page
+	 */
 	public static function getInfoByURL() {
 		global $APPLICATION;
 		$currentUrl = $APPLICATION->GetCurPage();
@@ -168,33 +133,88 @@ class CRockotEventHandlers
 		return $result;
 	}
 
-	public static function getGroupId() {
-
+	/**
+	 * Get page type
+	 */
+	public static function getPageType() {
+		return RockotRequestHelper::getInfoByURL()["type"];
 	}
 
-
-	public static function getDealLinkTemplate2($link, $title) {
-		return '
-			<div class="main-buttons-item" id="" data-disabled="false" data-class="" data-id="" data-item="" data-top-menu-id="" title="" draggable="true" tabindex="-1" data-link="">
-				<a href="'.$link.'" class="main-buttons-item-link">
-					<span class="main-buttons-item-icon"></span>
-					<span class="main-buttons-item-text">
-						<span class="main-buttons-item-drag-button" data-slider-ignore-autobinding="true"></span>
-						<span class="main-buttons-item-text-title">
-							<span class="main-buttons-item-text-box">
-								'.$title.'
-								<span class="main-buttons-item-menu-arrow"></span>
-							</span>
-						</span>
-						<span class="main-buttons-item-edit-button" data-slider-ignore-autobinding="true"></span>
-						<span class="main-buttons-item-text-marker"></span>
-					</span>
-					<span class="main-buttons-item-counter"></span>
-				</a>
-			</div>
-		';
+	/**
+	 * Get page ID
+	 */
+	public static function getPageId() {
+		return RockotRequestHelper::getInfoByURL()["id"];
 	}
 
+	/**
+	 * Check is ajax request
+	 */
+	public static function isAjaxRequest() {
+		if (isset($_REQUEST["ajax_request"]) && $_REQUEST["ajax_request"] === "Y") {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Check is iframe page
+	 */
+	public static function isPageInIframe() {
+		$request = \Bitrix\Main\Context::getCurrent()->getRequest();
+		$queryList = $request->getQueryList()->toArray();
+		return $queryList["IFRAME"] == "Y";
+	}
+
+}
+
+class RockotDeal {
+
+	/**
+	 * Get group and disk links for deal
+	 */
+	static public function getGroupAndDiskLinksByDeal($dealId) {
+		$result = ["group" => null, "disk" => null];
+		CModule::IncludeModule('crm');
+		$dbRes = CCrmDeal::GetListEx(
+				[],
+				["ID" => $dealId],
+				false,
+				false,
+				[
+					"UF_CRM_1679410842", // Group link
+					"UF_CRM_1679410808" // Disk link
+				]
+		);
+		while ($deal = $dbRes->Fetch()) {
+				$result["group"] = $deal["UF_CRM_1679410842"];
+				$result["disk"] = $deal["UF_CRM_1679410808"];
+		}
+		return $result;
+	}
+
+	/**
+	 * Get list of BP for deal
+	 */
+	static public function getBpListByDealId() {
+		$result = [];
+		if (!CModule::IncludeModule("bizproc") || !CModule::IncludeModule("crm")) {
+			return $result;
+		}
+		$documentType = ['crm', 'CCrmDocumentDeal', 'DEAL'];
+		$documentId = 'DEAL_'.$dealId;
+
+		$dbRes = CBPStateService::GetDocumentStates($documentType, $documentId);
+
+		foreach ($documentStates as $state) {
+			array_push($stack, $state);
+		}
+		return $result;
+	}
+
+	/**
+	 * Get HTML template for menu item
+	 */
 	public static function getDealLinkTemplate($link, $title) {
 		return '
 			<div class="main-buttons-item"  title="">
@@ -217,8 +237,72 @@ class CRockotEventHandlers
 		</div>';
 	}
 
-	//---------
+	/**
+	 * Get HTML template for menu item v2
+	 */
+	public static function getDealLinkTemplate2($link, $title) {
+		return '
+			<div class="main-buttons-item" id="" data-disabled="false" data-class="" data-id="" data-item="" data-top-menu-id="" title="" draggable="true" tabindex="-1" data-link="">
+				<a href="'.$link.'" class="main-buttons-item-link">
+					<span class="main-buttons-item-icon"></span>
+					<span class="main-buttons-item-text">
+						<span class="main-buttons-item-drag-button" data-slider-ignore-autobinding="true"></span>
+						<span class="main-buttons-item-text-title">
+							<span class="main-buttons-item-text-box">
+								'.$title.'
+								<span class="main-buttons-item-menu-arrow"></span>
+							</span>
+						</span>
+						<span class="main-buttons-item-edit-button" data-slider-ignore-autobinding="true"></span>
+						<span class="main-buttons-item-text-marker"></span>
+					</span>
+					<span class="main-buttons-item-counter"></span>
+				</a>
+			</div>
+		';
+	}
 
+	/**
+	 * Add link to deal menu
+	 */
+	public static function  addLinkToMenu($link, $title) {
+		?>
+		<script>
+        document.addEventListener('DOMContentLoaded', function() {
+						const customCardHtml = `<?=RockotDeal::getDealLinkTemplate2($link, $title)?>`;
+						const container = document.querySelector(".main-buttons-inner-container");
+						if (container) {
+							container.insertAdjacentHTML('afterbegin', customCardHtml);
+						}
+        });
+    </script>
+		<?
+	}
+}
+
+class RockotGroup {
+
+	/**
+	 * Add link to group menu
+	 */
+	public static function addLinkToGroupMenu($link, $title) {
+		?>
+		<script>
+        document.addEventListener('DOMContentLoaded', function() {
+						const customCardHtml = `<?=RockotGroup::getGroupLinkTemplate($link, $title)?>`;
+						const container = document.querySelector(".main-buttons-inner-container");
+						console.log(container);
+						if (container) {
+							container.insertAdjacentHTML('beforeend', customCardHtml);
+						}
+        });
+    </script>
+		<?
+	}
+
+	/**
+	 * Get HTML template for menu item 
+	 */
 	public static function getGroupLinkTemplate($link, $title) {
 		return '
 			<div class="main-buttons-item tasks_role_link" id="" data-disabled="false" data-class="" data-id="view_role_auditor" data-locked="" data-top-menu-id="group_panel_menu_1" data-parent-item-id="view_all" title="" draggable="true" tabindex="-1" data-link="item-jco2rlut">
@@ -236,120 +320,21 @@ class CRockotEventHandlers
 			</div>
 		';
 	}
-
 }
 
-function getItemById($itemId) {
-	// UF_CRM_1679410842
-	if (CModule::IncludeModule('socialnetwork')) {
-    $arGroup = CSocNetGroup::GetByID($itemId);
-    if ($arGroup) {
-				// echo "<pre>";
-        // print_r($arGroup);
-				// echo "</pre>";
-				// die();
-    }
+class RockotDebugger {
+
+	/**
+	 * Print message in log file
+	 */
+	static public function print($message) {
+		file_put_contents($_SERVER['DOCUMENT_ROOT']."/deb.log", $mes."\n", FILE_APPEND);
 	}
-	$deal = getDealForGroup($itemId);
-	return $deal;
 }
 
-function getDealForGroup($groupId) {
-	if (CModule::IncludeModule('tasks') && CModule::IncludeModule('crm')) {
-    $dbRes = CTasks::GetList(
-        [],
-        ["GROUP_ID" => $groupId],
-        ["UF_CRM_TASK"]
-    );
-    while ($task = $dbRes->Fetch()) {
-        // print_r($task);
-				foreach ($task['UF_CRM_TASK'] as $crmElement) {
-					if (strpos($crmElement, 'D_') === 0) {
-							$dealId = substr($crmElement, 2); // Извлекаем ID сделки
-							$deal = CCrmDeal::GetByID($dealId);
-							if ($deal) {
-									// print_r($deal);
-									// echo "Название сделки: " . $deal['TITLE'] . "<br>";
-									return $deal;
-							}
-					}
-			}
-    }
-	}
-	return null;
-}
-
-function getDiskByGroupId($groupId) {
-	if (!CModule::IncludeModule('disk')) {
-		// echo("none");
-    // die('Модуль "Диск" не найден');
-	}
-	$storage = \Bitrix\Disk\Driver::getInstance()->getStorageByGroupId($groupId);
-	if ($storage) {
-    $folder = $storage->getRootObject();
-    if ($folder) {
-        $urlManager = \Bitrix\Disk\Driver::getInstance()->getUrlManager();
-        // $folderUrl = $urlManager->encodeUrn($urlManager->getPathFolderList($folder->getStorageId(), $folder->getId()));
-        // echo "Ссылка на корневую папку диска: " . $folderUrl;
-    }
-	}
-	//
-	//https://dev24.icstar.ru/workgroups/group/46/disk/path/
-	//https://dev24.icstar.ru/workgroups/group/208/
-	// 5025
-	// https://dev24.icstar.ru/online/?IM_DIALOG=chat5025
-	// https://dev24.icstar.ru/workgroups/group/208/tasks/
-}
-
-function getDealUF($dealId) {
-	$result = ["group" => null, "disk" => null];
-	CModule::IncludeModule('crm');
-	$dbRes = CCrmDeal::GetListEx(
-			[],
-			["ID" => $dealId],
-			false,
-			false,
-			[
-				"UF_CRM_1679410842", // Group link
-				"UF_CRM_1679410808" // Disk link
-			]
-	);
-	while ($deal = $dbRes->Fetch()) {
-			$result["group"] = $deal["UF_CRM_1679410842"];
-			$result["disk"] = $deal["UF_CRM_1679410808"];
-	}
-	return $result;
-}
-
-function getBpListByDeal($dealId) {
-	$result = [];
-	if (!CModule::IncludeModule("bizproc") || !CModule::IncludeModule("crm")) {
-    return $result;
-	}
-	$documentType = ['crm', 'CCrmDocumentDeal', 'DEAL']; // Тип документа для сделки
-	$documentId = 'DEAL_'.$dealId; // Формат ID документа
-
-	$dbRes = CBPStateService::GetDocumentStates($documentType, $documentId);
-
-	// while ($arRes = $dbRes->GetNext()) {
-	// 		// Здесь у вас есть данные о каждом бизнес-процессе
-	// 		// echo "Название: " . $arRes['TEMPLATE_NAME'] . "<br>";
-	// 		// echo "Статус: " . $arRes['STATE_TITLE'] . "<br>";
-	// 		// ... И другая информация о бизнес-процессе
-	// 		array_push($stack, $arRes);
-	// }
-
-	foreach ($documentStates as $state) {
-		// Здесь у вас есть данные о каждом бизнес-процессе
-		echo "Название: " . $state['TEMPLATE_NAME'] . "<br>";
-		echo "Статус: " . $state['STATE_TITLE'] . "<br>";
-		// ... И другая информация о бизнес-процессе
-		array_push($stack, $state);
-}
-
-	return $result;
-}
-
-function _print_($mes) {
-	// file_put_contents("/Applications/MAMP/logs/deb.log", $mes."\n", FILE_APPEND);
-}
+//////////
+// https://dev24.icstar.ru/workgroups/group/46/disk/path/
+// https://dev24.icstar.ru/workgroups/group/208/
+// 5025
+// https://dev24.icstar.ru/online/?IM_DIALOG=chat5025
+// https://dev24.icstar.ru/workgroups/group/208/tasks/
