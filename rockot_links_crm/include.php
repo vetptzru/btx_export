aeb7df792a0791f12bab5c4bed4a2a417b66e516
vitaly.perov <vitaly.perov@ringcentral.com>
2023-12-20 15:54:04 +0300

    asd

--- rockot_links_crm/include.php
+++ rockot_links_crm/include.php
@@ -77,7 +77,7 @@ class CRockotEventHandlers
 function getItemById($groupId) {
 	if (CModule::IncludeModule('crm')) {
     // $filter = ['UF_CRM_1679410842' => $dealId];
-		$filter = ['!'.UF_GROUP => ''];
+		$filter = ['!UF_CRM_1679410842' => ''];
     $select = ['ID', 'TITLE', UF_GROUP]; // Поля, которые вы хотите получить
 
     $dbRes = CCrmDeal::GetListEx([], $filter, false, false, $select);
