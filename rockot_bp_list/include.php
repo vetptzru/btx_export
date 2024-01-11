540ee6ab2782fc91f93394eccae63b10fb0d45b1
vitaly.perov <vitaly.perov@ringcentral.com>
2024-01-11 22:13:06 +0300

    asd

--- rockot_bp_list/include.php
+++ rockot_bp_list/include.php
@@ -138,17 +138,17 @@ class CBpListEventHandlers
 
 		while ($row = $iterator->fetch()) {
 
-			$templateName = self::getBpTemplateName($row, $templateList);
-			$row["_TEMPLATE_NAME"] = $templateName;
+			// $templateName = self::getBpTemplateName($row, $templateList);
+			// $row["_TEMPLATE_NAME"] = $templateName;
 
-			$documentName = self::getBpDocumentName($row);
-			$row["_DOCUMENT_NAME"] = $documentName;
+			// $documentName = self::getBpDocumentName($row);
+			// $row["_DOCUMENT_NAME"] = $documentName;
 
-			$startedBy = self::getStartedBy($row);
-			$row["_STARTED_BY"] = $startedBy;
+			// $startedBy = self::getStartedBy($row);
+			// $row["_STARTED_BY"] = $startedBy;
 
-			$documentUrl = self::getBpDocumentUrl($row);
-			$row["DOCUMENT_URL"] = $documentUrl;
+			// $documentUrl = self::getBpDocumentUrl($row);
+			// $row["DOCUMENT_URL"] = $documentUrl;
 
 			self::dump($row);
 		}
